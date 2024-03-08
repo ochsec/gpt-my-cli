9bd135c7cb062d9dcfd41bf2bd2e0deb3183da7b
ochsec <chris.ochsenreither@tsgs.com>
2024-03-07 21:46:16 -0700

    updated

--- src/lib/methods/utils/concatFiles.ts
+++ src/lib/methods/utils/concatFiles.ts
@@ -1,10 +1,11 @@
 import { walk } from "https://deno.land/std@0.127.0/fs/mod.ts";
 import { extname } from "https://deno.land/std@0.127.0/path/mod.ts";
 
-export default async function concatFiles(dir: string, extensions: string[]): Promise<string> {
+export default async function concatFiles(args: string[]): Promise<string> {
+  const extensions = args[1].split(",");
   let contents = "";
 
-  for await (const entry of walk(dir, { includeDirs: false, followSymlinks: false })) {
+  for await (const entry of walk(args[0], { includeDirs: false, followSymlinks: false })) {
     if (extensions.includes(extname(entry.path))) {
       const fileContents = await Deno.readTextFile(entry.path);
       contents += fileContents;
