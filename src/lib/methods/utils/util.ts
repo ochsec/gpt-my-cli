f8e34935da41b4b97f6038f2df8db63c8852dd02
ochsec <chris.ochsenreither@tsgs.com>
2024-03-07 22:14:45 -0700

    unexpose htmlToMarkdown

--- src/lib/methods/utils/util.ts
+++ src/lib/methods/utils/util.ts
@@ -1,12 +1,10 @@
 import IUtil from "../../types/IUtil.d.ts";
 import fetchTextData from "./fetchTextData.ts";
-import htmlToMarkdown from "./htmlToMarkdown.ts";
 import makeDoc from "./makeDoc.ts";
 import concatFiles from "./concatFiles.ts";
 
 const Util: IUtil = {
     fetchTextData,
-    htmlToMarkdown,
     makeDoc,
     concatFiles,
 };
--- src/lib/types/IUtil.d.ts
+++ src/lib/types/IUtil.d.ts
@@ -1,6 +1,5 @@
 interface IUtil {
     fetchTextData (args: string[]): Promise<string>
-    htmlToMarkdown(html: string): string;
     makeDoc(args: string[]): Promise<string | null>;
     concatFiles(args: string[]): Promise<string>;
 }
