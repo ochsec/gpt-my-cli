107911db8746fc73d45c4006bfcdd69a271a8e35
ochsec <chris.ochsenreither@tsgs.com>
2024-03-07 18:21:24 -0700

    include message history in new completion

--- src/lib/methods/completion/complete.ts
+++ src/lib/methods/completion/complete.ts
@@ -1,22 +1,17 @@
 import { IMessageItem } from "../../types/History.d.ts";
 import State from "../../state/state.ts";
 import readLLMConfig from "../../state/readLLMConfig.ts";
-import IChatConfig from "../../types/IChatConfig.d.ts";
-import HistoryType from "../../types/History.d.ts";
 
 export default async function complete(args: string[]): Promise<string | undefined> {
     const openai = State.getOpenAIApi();
     const config = await readLLMConfig();
     if (config) {
         let message: IMessageItem;
-        const messages: Array<IMessageItem> = [];
-        // let history = State.getHistory();
-        // let history = [] as HistoryType[];
+        let messages: Array<IMessageItem> = [];
 
         let history = await State.getSession(config.session) || [];
 
         if (typeof args[0] === 'string') {
-            console.log("Entered type of input === 'string': ", args[0]);
             message = { role: 'user', content: args[0] };
             messages.push(message);
             history = [...history, ...messages];
@@ -25,15 +20,12 @@ export default async function complete(args: string[]): Promise<string | undefin
             history = [...history, ...args[0]];
         }
 
-        // State.setHistory(history);
-
         // Set memory
         if (history.length > config['memory']) {
-            history = history.slice(config['memory']);
+            messages = history.slice(config['memory']);
+        } else {
+            messages = history;
         }
-        // } else {
-        //     messages = history;
-        // }
 
         const { model, temperature, top_p, frequency_penalty, presence_penalty, max_tokens } = config;
 
@@ -46,11 +38,11 @@ export default async function complete(args: string[]): Promise<string | undefin
             max_tokens,
             messages: messages,
         };
-        // console.log(params);
+
         try {
             const response = await openai.createChatCompletion(params);
             const answer = response.data?.choices[0]?.message?.content;
-            console.log(answer);
+
             history.push({
                 role: 'system',
                 content: answer,
